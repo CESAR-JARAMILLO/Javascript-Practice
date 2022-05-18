22c18ddbe9d09fc1d78288f51bebf82ef97e264b
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2022-05-18 14:14:55 -0700

    using the console

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-alert('Hello World');
+// Log to console
+console.log('Hello World');
+console.log(123);
+console.log(true);
+console.log([1,2,3,4]);
+console.log({a:1, b:2});
+console.table({a:1, b:2});
+
+console.error('This is an error');
+console.clear();
+console.warn('This is a warning');
+console.time('Hello');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+console.timeEnd('Hello')
